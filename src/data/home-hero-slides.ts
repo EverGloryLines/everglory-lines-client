43e2c7e98f3d1361e03fc4997eab28636c3e23fb
Shimu-Afsana <160387753+Shimu-Afsana@users.noreply.github.com>
2025-12-23 18:07:56 +0600

    Update home page id-1 contact number in home page

--- src/data/home-hero-slides.ts
+++ src/data/home-hero-slides.ts
@@ -11,7 +11,7 @@ export const heroSlides: Slide[] = [
     image: "/homepage/hero/slide-two-image.png",
     ctaText: "Multimodal Services",
     ctaLink: "/our-services/multimodal-transport",
-    phoneNumber: "+880 1334-938912",
+    phoneNumber: "+880 1727-423016",
   },
   {
     id: "2",
