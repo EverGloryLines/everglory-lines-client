0c332153d09191d81fa267a96c758578f4938e0a
Shimu-Afsana <160387753+Shimu-Afsana@users.noreply.github.com>
2025-12-28 15:47:50 +0600

    changes fleet  vessel header  logo

--- src/app/(main)/fleet/vessel/_components/vessel-tabs/vessel-header.tsx
+++ src/app/(main)/fleet/vessel/_components/vessel-tabs/vessel-header.tsx
@@ -15,7 +15,7 @@ export function VesselHeader() {
 
       <div className="w-24 h-16 sm:w-40 sm:h-24 flex-shrink-0">
         <Image
-          src="/logo/everglory-Container-lines.png"
+          src="/logo/everglory-container-lines.png"
           alt="Everglory Container Lines Logo"
           width={80}
           height={80}
