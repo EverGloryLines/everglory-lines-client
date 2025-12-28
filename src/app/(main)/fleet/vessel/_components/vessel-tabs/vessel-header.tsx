b4f273cc5e8f0cb6767fe20645366eedf7cbcf88
Shimu-Afsana <160387753+Shimu-Afsana@users.noreply.github.com>
2025-12-28 15:33:43 +0600

    changes logo on fleet vessel header section everglory lines to everglory container lines

--- src/app/(main)/fleet/vessel/_components/vessel-tabs/vessel-header.tsx
+++ src/app/(main)/fleet/vessel/_components/vessel-tabs/vessel-header.tsx
@@ -15,7 +15,7 @@ export function VesselHeader() {
 
       <div className="w-24 h-16 sm:w-40 sm:h-24 flex-shrink-0">
         <Image
-          src="/logo/everglory-lines-logo.jpg"
+          src="/logo/everglory-Container-lines.png"
           alt="Everglory Container Lines Logo"
           width={80}
           height={80}
