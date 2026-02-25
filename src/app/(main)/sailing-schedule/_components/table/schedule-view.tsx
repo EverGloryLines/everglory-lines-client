4bf5c58caa52f1aabacd5cefde643aa4493eb47c
Shimu-Afsana <160387753+Shimu-Afsana@users.noreply.github.com>
2026-02-25 13:35:00 +0600

    Update schedule data change

--- src/app/(main)/sailing-schedule/_components/table/schedule-view.tsx
+++ src/app/(main)/sailing-schedule/_components/table/schedule-view.tsx
@@ -8,11 +8,11 @@ const dataBlock1: PortSchedule[] = [
   {
     vessel: "UDDIPON EXPRESS",
     voyage: "UDP-1",
-    port1: { eta: "26/12", etd: "28/12" }, // Chittagong
+    port1: { eta: "27/02", etd: "28/02" }, // Chittagong
     port2: { eta: "-", etd: "-" },         // Pangaon
-    port3: { eta: "31/12", etd: "03/01" }, // Kolkata
-    port4: { eta: "03/01", etd: "05/01" }, // Haldia
-    port5: { eta: "08/01", etd: "10/01" }, // Chittagong
+    port3: { eta: "03/03", etd: "04/03" }, // Kolkata
+    port4: { eta: "05/03", etd: "06/03" }, // Haldia
+    port5: { eta: "09/03", etd: "10/03" }, // Chittagong
   },
 ];
 
@@ -21,37 +21,37 @@ const dataBlock2: PortSchedule[] = [
   {
     vessel: "UDDIPON EXPRESS",
     voyage: "UDP-2",
-    port1: { eta: "08/01", etd: "10/01" }, // Chittagong
-    port2: { eta: "12/01", etd: "14/01" }, // Haldia
-    port3: { eta: "16/01", etd: "18/01" }, // Pangaon
+    port1: { eta: "-", etd: "-" }, // Chittagong
+    port2: { eta: "-", etd: "-" }, // Haldia
+    port3: { eta: "-", etd: "-" }, // Pangaon
     port4: { eta: "-", etd: "-" },         // Haldia
-    port5: { eta: "19/01", etd: "21/01" }, // Chittagong
+    port5: { eta: "-", etd: "-" }, // Chittagong
   },
   {
     vessel: "UDDIPON EXPRESS",
     voyage: "UDP-3",
-    port1: { eta: "19/01", etd: "21/01" },
-    port2: { eta: "23/01", etd: "25/01" },
-    port3: { eta: "27/01", etd: "29/01" },
-    port4: { eta: "01/02", etd: "03/02" },
+    port1: { eta: "-", etd: "-" },
+    port2: { eta: "-", etd: "-" },
+    port3: { eta: "-", etd: "-" },
+    port4: { eta: "-", etd: "-" },
     port5: { eta: "-", etd: "-" },
   },
   {
     vessel: "UDDIPON EXPRESS",
     voyage: "UDP-4",
     port1: { eta: "-", etd: "-" },
-    port2: { eta: "01/02", etd: "04/02" },
-    port3: { eta: "06/02", etd: "08/02" },
-    port4: { eta: "10/02", etd: "13/02" },
+    port2: { eta: "-", etd: "-" },
+    port3: { eta: "-", etd: "-" },
+    port4: { eta: "-", etd: "-" },
     port5: { eta: "-", etd: "-" },
   },
   {
     vessel: "UDDIPON EXPRESS",
     voyage: "UDP-5",
     port1: { eta: "-", etd: "-" },
-    port2: { eta: "10/02", etd: "13/02" },
-    port3: { eta: "15/02", etd: "17/02" },
-    port4: { eta: "19/02", etd: "22/02" },
+    port2: { eta: "-", etd: "-" },
+    port3: { eta: "-", etd: "-" },
+    port4: { eta: "-", etd: "-" },
     port5: { eta: "-", etd: "-" },
   },
 ];
